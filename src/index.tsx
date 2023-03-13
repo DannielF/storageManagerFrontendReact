b9e138d11aa0bde25db17b16579984dfa1beece6
daniel.granados <daniel.granados@sofka.com.co>
2023-03-13 10:06:22 -0500

    Add: redux store

--- src/index.tsx
+++ src/index.tsx
@@ -5,6 +5,8 @@ import { BrowserRouter } from "react-router-dom";
 import "./index.scss";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import { Provider } from "react-redux";
+import { Store } from "./shared/store/Store";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
@@ -12,7 +14,9 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <Provider store={Store}>
+        <App />
+      </Provider>
     </BrowserRouter>
   </React.StrictMode>
 );
